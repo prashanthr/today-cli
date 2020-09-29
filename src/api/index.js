c398fded9b740e71dfb83ccd22343d912e111fe7
Prashanth R <1409501+prashanthr@users.noreply.github.com>
2020-09-28 17:34:19 -0700

    read and write config to home dir

--- src/api/index.js
+++ src/api/index.js
@@ -1,7 +1,7 @@
 const axios = require('axios')
 const { get } = require('lodash')
 const { isProd, getEnv } = require('../util/env')
-const fs = require('fs')
+const { CONFIG_FILE_NAME, getHomeFilePath, writeToFile, readFromFile } = require('../util/file')
 const { getLocationFromIp } = require('../util/location')
 
 const weatherIconFolder = '/tmp/today'
@@ -21,48 +21,25 @@ const buildInitialState = (data) => {
 	}
 }
 
-const getDataUrl = async (params = {}) => {
-	const { weatherUnit, historyLimit, newsLimit, country } = params
-	const location = await getLocationFromIp() || params.location
+const getDataUrl = (params = {}) => {
+	const { weatherUnit, historyLimit, newsLimit, country, location } = params
 	const baseUrl = isProd()
 		? getEnv('TODAY_API_HOST')
 		: `http://${getEnv('TODAY_API_HOST')}:${getEnv('TODAY_API_PORT')}`
 	return `${baseUrl}/today?location=${encodeURIComponent(location)}&country=${country}&wod_unit=${weatherUnit}&hod_limit=${historyLimit}&nod_limit=${newsLimit}`
 }
 
-const downloadFile = async (url, path) => {
-	const res = await axios({
-    method: 'get',
-    url,
-    responseType: 'stream'
-	})
-
-	// console.log('url', url, 'res', res)
-	const writer = fs.createWriteStream(path)
-	// await fs.promises.mkdir(weatherIconFolder)
-
-	res.data.pipe(writer)
-
-  return new Promise((resolve, reject) => {
-    writer.on('finish', resolve)
-    writer.on('error', reject)
-	})
-
-	// await res.data.pipe(
-	// 	fs.createWriteStream(path)
-	// )
-
-}
-
 const getData = async (params) => {
 	try {
-		const res = await axios.get(await getDataUrl(params))
-		const weatherIcon = get(res.data, 'wod.weather[0].icon', undefined)
-		if (weatherIcon) {
-			await downloadFile(weatherIcon, weatherIconPath)
+		const configFilePath = getHomeFilePath(CONFIG_FILE_NAME)
+		const readParams = await readFromFile(configFilePath)
+		const resolvedParams = readParams || {
+			...params,
+			location: await getLocationFromIp() || params.location
 		}
-		// console.log('res', res, res.data)
-		return adaptDataForClient({ initData: params, data: res.data })
+		const res = await axios.get(getDataUrl(resolvedParams))
+		await writeToFile(resolvedParams, configFilePath)
+		return adaptDataForClient({ initData: resolvedParams, data: res.data })
 	} catch (err) {
 		console.error('Error fetching data', err)
 		return {}
