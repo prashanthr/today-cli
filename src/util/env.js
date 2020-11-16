e7954f649e36cd15a812d998d92276eca02b198b
Prashanth R <1409501+prashanthr@users.noreply.github.com>
2020-11-15 17:18:54 -0800

    fix(env): Fix prod env check

--- src/util/env.js
+++ src/util/env.js
@@ -2,6 +2,6 @@ const { get, set } = require('lodash')
 const getEnv = (key, defaultValue) => get(process.env, key, defaultValue)
 const setEnv = (key, value) => set(process.env, key, value)
 const getNodeEnv = () => getEnv('NODE_ENV', 'development')
-const isProd = () => getNodeEnv().toLowerCase === 'production'
+const isProd = () => getNodeEnv().toLowerCase() === 'production'
 
 module.exports = { isProd, getEnv, setEnv }
