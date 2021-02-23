d5e008866bb73575fbb3768b3de33f0c6ac3cebc
Prashanth R <1409501+prashanthr@users.noreply.github.com>
2021-02-23 15:00:25 -0800

    feat(song-of-the-day): Add song of the day

---
+++ src/ui/components/song.js
@@ -0,0 +1,43 @@
+'use strict'
+const React = require('react')
+const PropTypes = require('prop-types')
+const importJsx = require('import-jsx')
+const { Text, Box } = require('ink')
+const Link = require('ink-link')
+const Heading = importJsx('./heading')
+const LinkItem = importJsx('./link-item')
+const { capitalizeText } = require('../../util/text')
+
+const Song = ({ data, colors }) => (
+	<>
+		{data && (
+			<>
+				<Heading text={`Song of the day`} colors={colors} />
+				<LinkItem
+					title={
+							<Text color={colors.primary}>
+								<Text color={colors.secondary}>{data.track_name}</Text>
+								&nbsp;by <Text color={colors.primary}>{data.artist_name}</Text>
+								&nbsp;on <Text color={colors.tertiary}>{capitalizeText(data.source)}</Text>
+							</Text>
+					}
+					url={data.uri}
+					linkText={'Listen'}
+					colors={colors}
+				/>
+			</>
+		)}
+	</>
+)
+
+Song.propTypes = {
+	data: PropTypes.shape({
+		artist_name: PropTypes.string,
+		track_name: PropTypes.string,
+    uri: PropTypes.string,
+    source: PropTypes.string
+	})
+}
+
+
+module.exports = Song
